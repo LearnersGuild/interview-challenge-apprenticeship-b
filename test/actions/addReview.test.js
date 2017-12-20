6316e85bb55930fcd6d0910ea8e1871fd111bf47
Punit Rathore <punitr@gmail.com>
2017-12-20 10:21:59 -0800

    fixing test name

--- test/actions/addReview.test.js
+++ test/actions/addReview.test.js
@@ -5,7 +5,7 @@ import { addReview } from '../../src/actions'
 dotenv.config()
 
 describe('function addReview ', () => {
-  it('should create a new row in the ratings table', () => {
+  it('should create a new row in the reviews table', () => {
     // TODO: FIXME and write a real test
     expect(1).to.equal(2)
   })
